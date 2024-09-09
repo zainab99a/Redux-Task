fd5116f100e4eedc83c596dc790c074261886e23
zainab99a <amanhal729@gmail.com>
2024-09-09 20:13:29 +0300

    add action

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+
 import { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { DECREMENT, INCREMENT } from './redux/Actions'
 export default function Counter() {
     const [count, setcount] = useState(0)
+    const stateCount=useSelector(state=>state.count)
+    console.log(stateCount)
+    const dispatch=useDispatch()
   return (
     <div>Counter
-    <p>{count}</p>
-    <button onClick={()=>{setcount(count+1)}}>increment</button>
-    <button  onClick={()=>{setcount(count>0?count-1 :count)}}>decrement</button></div>
+    <p>{stateCount}</p>
+    <button onClick={()=>{dispatch({type:INCREMENT})}}>increment</button>
+    <button  onClick={()=>{dispatch({type:DECREMENT})}}>decrement</button></div>
   )
 }
--- src/redux/Reducer.jsx
+++ src/redux/Reducer.jsx
@@ -1,4 +1,10 @@
-const counterReducer=(state ={},action)=>{
-return state
+import { DECREMENT, INCREMENT } from "./Actions"
+
+const counterReducer=(state ={count:0},action)=>{
+    if(action.type===INCREMENT){
+return {count:state.count+1}}
+if(action.type===DECREMENT){
+    return {count:state.count-1}}
+    return state
 }
 export default counterReducer
